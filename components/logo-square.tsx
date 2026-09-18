9aec856793ab557b40d25e362570d85887cd1c43
Leonardo Rodrigues <leorodrigues1036@gmail.com>
2026-09-18 17:38:43 -0300

    minhas alterações no layout e logo

--- components/logo-square.tsx
+++ components/logo-square.tsx
@@ -7,13 +7,14 @@ export default function LogoSquare({ size }: { size?: "sm" | undefined }) {
       className={clsx(
         "flex flex-none items-center justify-center border border-neutral-200 bg-white dark:border-neutral-700 dark:bg-black",
         {
-          "h-[40px] w-[40px] rounded-xl": !size,
-          "h-[30px] w-[30px] rounded-lg": size === "sm",
+          "h-[50px] w-[50px] rounded-xl": !size,
+          "h-[40px] w-[40px] rounded-lg": size === "sm",
         },
       )}
     >
       <LogoIcon
-        className={clsx({
+        className={clsx( "flex items-center justify-center ",
+          {
           "h-[16px] w-[16px]": !size,
           "h-[10px] w-[10px]": size === "sm",
         })}
